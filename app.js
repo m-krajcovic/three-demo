5719e717ff3dd631400fbc5ad258be75c93e1461
Michal Krajcovic <mich.krajcovic@gmail.com>
2017-10-05 01:16:11 +0200

    added shockwave on click

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 const Follower = (function(mesh, goal, movementSpeed, rotationStep) {
     let movementVector = new THREE.Vector2(0, 1);
     const center = new THREE.Vector2(0, 0);
+    const maxSpeed = 2 * movementSpeed;
+    const defaultSpeed = movementSpeed;
     function getNewMovementVector() {
         const currentVector = movementVector.clone();
         const plusVector = movementVector.clone().rotateAround(center, rotationStep * Math.PI/180);
         const minusVector = movementVector.clone().rotateAround(center, -rotationStep * Math.PI/180);
         const distances = [
-            currentVector.clone().add(mesh.position).distanceToSquared(goal), 
-            plusVector.clone().add(mesh.position).distanceToSquared(goal), 
-            minusVector.clone().add(mesh.position).distanceToSquared(goal)];
+            currentVector.clone().multiplyScalar(movementSpeed).add(mesh.position).distanceToSquared(goal), 
+            plusVector.clone().multiplyScalar(movementSpeed).add(mesh.position).distanceToSquared(goal), 
+            minusVector.clone().multiplyScalar(movementSpeed).add(mesh.position).distanceToSquared(goal)];
         const minIndex = utils.indexOfMin(distances);
         return [currentVector, plusVector, minusVector][minIndex];
     }
 
+    const previousMovementVector = new THREE.Vector2();
+
     return {
-        next: function() {
+        next: function(additionalVectors) {
             if (goal.distanceTo(mesh.position) > 1) {
-                movementVector.copy(getNewMovementVector());
-                mesh.position.x += movementVector.x * movementSpeed;
-                mesh.position.y += movementVector.y * movementSpeed;
+                const newVector = getNewMovementVector();
+                if (additionalVectors && additionalVectors.length) {
+                    additionalVectors.forEach(vector => {
+                        newVector.add(vector);
+                    });
+                }
+                mesh.position.x += newVector.x * movementSpeed;
+                mesh.position.y += newVector.y * movementSpeed;
+
+                if (movementVector.equals(newVector) || previousMovementVector.equals(newVector)) {
+                    if (movementSpeed < maxSpeed) {
+                        movementSpeed *= 1.03;
+                        if (movementSpeed > maxSpeed) {
+                            movementSpeed = maxSpeed;
+                        }
+                    }
+                } else if (movementSpeed > defaultSpeed) {
+                    movementSpeed /= 1.04;
+                    if (movementSpeed < defaultSpeed) {
+                        movementSpeed = defaultSpeed;
+                    }
+                }
+                previousMovementVector.copy(movementVector);
+                movementVector.copy(newVector);
                 // mesh.rotation.z = movementVector.angle() - Math.PI/2;
             }
+        },
+
+        position: function() {
+            return mesh.position;
         },
 
         speed: function(value) {
@@ -53,6 +82,42 @@ const Follower = (function(mesh, goal, movementSpeed, rotationStep) {
     }
 });
 
+// create shockwave on position where i clicked
+// every tick, increase radius and check if shockwave contains an object:
+// if it does: move the object away from the position // might be stronger smaller the distance? (check later)
+// if it doesn't: don't do anything
+const ShockWave = (function(position, speed, strength, maxSize) {
+    
+    const maxRadius = maxSize;
+    let done = false;
+    const sphere = new THREE.Sphere(new THREE.Vector3(position.x, position.y, 0), 3);
+    return {
+        next: function() {
+            if (sphere.radius < maxRadius) {
+                sphere.radius *= speed;
+            } else {
+                done = true;
+            }
+        },
+
+        pushVector: function(vector) {
+            if (!done) {
+                const vectorDistance = sphere.distanceToPoint(vector);
+                if (vectorDistance <= 0) {
+                    const pushX = vector.x - sphere.center.x;
+                    const pushY = vector.y - sphere.center.y;
+                    vector.x += pushX * strength * ((-1) * vectorDistance/5 + 1);
+                    vector.y += pushY * strength * ((-1) * vectorDistance/5 + 1);
+                }
+            }
+        },
+
+        isDone: function() {
+            return done;
+        }
+    }
+});
+
 (function() {
 let scene = new THREE.Scene();
 const width = window.innerWidth;
@@ -78,6 +143,8 @@ let goal = new THREE.Vector2();
 
 renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
 renderer.domElement.addEventListener('touchmove', onDocumentMouseMove, false);
+
+renderer.domElement.addEventListener('mousedown', onDocumentMouseDown, false);
 window.addEventListener('resize', onWindowResize, false);
 
 function createCone(color) {
@@ -87,9 +154,13 @@ function createCone(color) {
     scene.add(cube);
     return cube;
 }
+const shockwaves = [];
+function onDocumentMouseDown(event) {
+    shockwaves.push(new ShockWave(goal, 1.1, 0.1, 60));
+}
 
 function onDocumentMouseMove(event) {
-    console.log(event);
+    // console.log(event);
     event.preventDefault();
     let mouseX, mouseY;
     if (event.touches && event.touches.length) {
@@ -130,6 +201,8 @@ const followers = [];
 const c1 = new THREE.Color(0xff4e50);
 const c2 = new THREE.Color(0xf9d423);
 // #ff4e50 → #f9d423
+// const c1 = new THREE.Color(0xff0000);
+// const c2 = new THREE.Color(0x0000ff);
 
 function createColorRange(ran) {
     const tmpColor = new THREE.Color();
@@ -154,10 +227,23 @@ function animate() {
     requestAnimationFrame(animate);
 
     manageRaycasterIntersections(scene, camera);
-
+    shockwaves.forEach(shockwave => {
+        shockwave.next();
+    });
     followers.forEach(follower => {
+        shockwaves.forEach(shockwave => {
+            shockwave.pushVector(follower.position());
+        });
         follower.next();
     });
+
+    if (shockwaves.length) {
+        for (let i = shockwaves.length - 1; i >= 0; i--) {
+            if (shockwaves[i].isDone()) {
+                shockwaves.splice(i, 1);
+            }
+        }
+    }
 
     renderer.render(scene, camera);
 }
